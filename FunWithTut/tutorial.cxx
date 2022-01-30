a9405d3d54082bac231c92bfad1dfbda7bedf71a
the_cowboy <ravimohan1991@gmail.com>
2022-01-31 03:25:06 +0530

    Introducing static library and relevant linkage
    Continuing with the official tutorial, we now add the static library building and linking CMake texts.

--- FunWithTut/tutorial.cxx
+++ FunWithTut/tutorial.cxx
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <string>
 #include <TutorialConfig.h>
+#ifdef USE_MYMATH
+#	include "MathFunctions.h"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -17,8 +20,13 @@ int main(int argc, char* argv[])
   // convert input to double
   const double inputValue = std::stod(argv[1]);
 
-  // calculate square root
+  // calculate square root with our own library as an option
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
   const double outputValue = sqrt(inputValue);
+#endif
+
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
