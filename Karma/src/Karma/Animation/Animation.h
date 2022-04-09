cd220220244c26071f873bcbc606a785068f44b1
winwinwindow <ravimohan1991@gmail.com>
2022-04-10 04:52:00 +0530

    Basic Animation abstractions setup

---
+++ Karma/src/Karma/Animation/Animation.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Karma/Core.h"
+
+namespace Karma
+{
+	class KARMA_API Animation
+	{
+		// Types of Animation 1. Sprites, 2. Rigged (with bones hierarchy and stuff)
+
+		// Ways to talk to the Renderer and main game loop hookup to actually display animating stuff
+
+	};
+
+}
---
+++ Karma/src/Karma/Animation/Animator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Karma/Core.h"
+
+namespace Karma
+{
+	// Anu-praan-aa - She who breathes (animated) life.
+	class KARMA_API Anuprana
+	{
+		// Mechanism for updating the animation time and determining the current pose (sequence, series of vertices transforms)
+		// and set the transforms in the bone-joint model.
+	};
+}
