ed0218430b01ab30ff2004e8edf352c60a84cf27
trueGriff <ravimohan1991@gmail.com>
2022-06-22 12:02:32 +0530

    Removed premake redundancies resulting from CMake transition
    1. Removed premake binaries
    2. Enhanced CMake build parameters for comfortable dev processing.
    3. Rigged CMake scripts with Dino art!

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,2 +1,15 @@
-call vendor\bin\Premake\Windows\premake5.exe vs2022
+@echo off
+mkdir KarmaLightHouse
+cd KarmaLightHouse
+cmake ../
+
+echo DO YOU WANT TO OPEN THE SOLUTION?
+set /p Input=Enter y(Yes) or n(No):
+If /I "%Input%"=="y" goto yes
+goto no
+:yes
+KarmaBasedProjectName.sln
+exit
+:no
+echo Suit yourself! Goodbye!
 PAUSE
