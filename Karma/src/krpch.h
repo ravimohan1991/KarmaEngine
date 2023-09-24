3851aa1502660fb2e5e9cb32fca9d19a2f02b751
BellInequality <ravimohan1991@gmail.com>
2023-09-25 03:44:47 +0530

    simplifying web of includes

--- Karma/src/krpch.h
+++ Karma/src/krpch.h
@@ -3,15 +3,11 @@
 // Utilities
 #include <iostream>
 #include <memory>
-#include <utility>
-#include <algorithm>
 #include <functional>
 #include <filesystem>
-
 #include <string>
 #include <sstream>
 #include <array>
-#include <vector>
 #include <unordered_map>
 #include <unordered_set>
 #include <stdint.h>
@@ -19,7 +15,6 @@
 #include <list>
 #include <utility>
 #include <set>
-#include <cstdint>
 #include <optional>
 #include <fstream>
 #include <ctype.h>
@@ -32,16 +27,6 @@
 #include <stdlib.h>
 #include <math.h>
 #include <limits.h>
-
-// No clue about this ImGui include. May or maynot be excluded in future.
-#if defined(_MSC_VER) && _MSC_VER <= 1500 // MSVC 2008 or earlier
-#include <stddef.h>
-#else
-#include <stdint.h>
-#endif
-
-// Intel intrinsics?
-//#include <immintrin.h>
 
 // Karma's general includes
 #include "Karma/Core.h"
