b7d97fb619b825146ce4f4c6a60eab10a3ff8e99
BellInequality <ravimohan1991@gmail.com>
2023-11-12 03:25:22 +0530

    Engine folder file documentation complete, more or less

--- Karma/src/Karma/Engine/GameViewportClient.h
+++ Karma/src/Karma/Engine/GameViewportClient.h
@@ -1,3 +1,13 @@
+/**
+ * @file GameViewportClient.h
+ * @author Ravi Mohan (the_cowboy)
+ * @brief This file contains the class UGameViewportClient..
+ * @version 1.0
+ * @date September 12, 2023
+ *
+ * @copyright Karma Engine copyright(c) People of India
+ */
+
 #pragma once
 
 #include "krpch.h"
@@ -7,8 +17,9 @@
 namespace Karma
 {
 	/**
-	 * A game viewport (FViewport) is a high-level abstract interface for the
+	 * @brief A game viewport (FViewport) is a high-level abstract interface for the
 	 * platform specific rendering, audio, and input subsystems.
+	 *
 	 * GameViewportClient is the engine's interface to a game viewport.
 	 * Exactly one GameViewportClient is created for each instance of the game.  The
 	 * only case (so far) where you might have a single instance of Engine, but
@@ -16,9 +27,7 @@ namespace Karma
 	 * you have more than one PIE window running.
 	 *
 	 * Responsibilities:
-	 * propagating input events to the global interactions list
-	 *
-	 * @see UGameViewportClient
+	 * 1. propagating input events to the global interactions list
 	 */
 	class UGameViewportClient : public UObject
 	{
@@ -27,6 +36,5 @@ namespace Karma
 	protected:
 		/* The relative world context for this viewport */
 		class UWorld* m_World;
-
 	};
 }
