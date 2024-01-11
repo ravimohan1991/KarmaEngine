bc3b3c69e251688ba56c136554111a7c2720111b
BellInequality <ravimohan1991@gmail.com>
2024-01-11 16:01:47 +0530

    VertexArray documentation

--- Karma/src/Karma/Renderer/VertexArray.h
+++ Karma/src/Karma/Renderer/VertexArray.h
@@ -33,7 +33,20 @@ namespace Karma
 		{
 		}
 
+		/**
+		 * @brief For binding OpenGL vertex array object
+		 *
+		 * @todo Seems only specific to OpenGL. Need to think the abstraction with Vulkan in mind
+		 * @since Karma 1.0.0
+		 */
 		virtual void Bind() const = 0;
+
+		/**
+		 * @brief Undo what Bind does
+		 *
+		 * @todo Again, need to think the abstraction
+		 * @since Karma 1.0.0
+		 */
 		virtual void UnBind() const = 0;
 
 		// For legacy purposes. Use Mesh abstraction.
@@ -41,20 +54,52 @@ namespace Karma
 		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) = 0;
 		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const = 0;
 		virtual const IndexBuffer* GetIndexBuffer() const = 0;
+		// end legacy purpose context
 
+		/**
+		 * @brief Sets the index and vertex buffers seperately
+		 *
+		 * @param mesh					A collection of index and vertex buffers to be set
+		 * @since Karma 1.0.0
+		 */
 		virtual void SetMesh(std::shared_ptr<Mesh> mesh) = 0;
 
 		// For legacy purposes. Use Material abstraction.
 		virtual void SetShader(std::shared_ptr<Shader> shader) = 0;
 
+		/**
+		 * @brief Sets the material
+		 *
+		 * @param material				Shader and texture setting
+		 * @todo a seperate setting of shader may be redundant
+		 *
+		 * @since Karma 1.0.0
+		 */
 		virtual void SetMaterial(std::shared_ptr<Material> material) = 0;
 
+		/**
+		 * @brief Called each game loop for updating
+		 *
+		 * @note includes uniform updation  (vertex coordinates updation in shader by view and projection matrices)
+		 * @since Karma 1.0.0
+		 */
 		virtual void UpdateProcessAndSetReadyForSubmission() const = 0;
 
 		// Getters
 		//virtual std::shared_ptr<Mesh> GetMesh() const = 0;
+
+		/**
+		 * @brief Getter function for material
+		 *
+		 * @since Karma 1.0.0
+		 */
 		virtual std::shared_ptr<Material> GetMaterial() const = 0;
 
+		/**
+		 * @brief Create the VertexArray object based on the rendering API chosen by the programmer
+		 *
+		 * @since Karma 1.0.0
+		 */
 		static VertexArray* Create();
 	};
 }
