2c23527f33d6ca8fdbc403d19f639fb94964971d
BellInequality <ravimohan1991@gmail.com>
2024-08-04 15:33:58 +0530

    testing ios cmake toolchain

--- GenerateProjects.sh
+++ GenerateProjects.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir KarmaLightHouse
 cd KarmaLightHouse
-cmake -DCMAKE_BUILD_TYPE=Debug -G Xcode ..
+cmake -DCMAKE_TOOLCHAIN_FILE=KarmaCmake/ios-cmake/ios.toolchain.cmake -GXcode -DPLATFORM=OS64COMBINED -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=RaviMohan -DVulkan_LIBRARY=/usr/local/lib/libvulkan.dylib -DOPENGL_gl_LIBRARY=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.5.sdk/System/Library/Frameworks/OpenGL.framework -DOPENGL_INCLUDE_DIR=/opt/X11/include ..
