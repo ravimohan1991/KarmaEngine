c8859af096193c519273f96440a00d451e0c2e07
ravimohan1991 <ravimohan1991@users.noreply.github.com>
2025-11-24 10:32:08 +0000

    deploy: 917ab3e4068a7e5dab34758af72ce49a15f9a45c

--- search/classes_2.js
+++ search/classes_2.js
@@ -4,5 +4,6 @@ var searchData=
   ['camera_1',['Camera',['../class_karma_1_1_camera.html',1,'Karma']]],
   ['controllerdevice_2',['ControllerDevice',['../class_karma_1_1_controller_device.html',1,'Karma']]],
   ['controllerdeviceconnectedevent_3',['ControllerDeviceConnectedEvent',['../class_karma_1_1_controller_device_connected_event.html',1,'Karma']]],
-  ['controllerdevicedisconnectedevent_4',['ControllerDeviceDisconnectedEvent',['../class_karma_1_1_controller_device_disconnected_event.html',1,'Karma']]]
+  ['controllerdevicedisconnectedevent_4',['ControllerDeviceDisconnectedEvent',['../class_karma_1_1_controller_device_disconnected_event.html',1,'Karma']]],
+  ['cpu_5',['CPU',['../struct_karma_1_1_karma_turing_machine_electronics_1_1_c_p_u.html',1,'Karma::KarmaTuringMachineElectronics']]]
 ];
