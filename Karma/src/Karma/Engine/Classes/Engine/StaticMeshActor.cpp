fe759a942f60bce0501d5f8f47e04f6a68a9e5e0
pc_boy <ravimohan1991@gmail.com>
2025-12-08 09:03:14 +0530

    Added staticmeshactor

---
+++ Karma/src/Karma/Engine/Classes/Engine/StaticMeshActor.cpp
@@ -0,0 +1,14 @@
+#include "StaticMeshActor.h"
+
+namespace Karma
+{
+	AStaticMeshActor::AStaticMeshActor(const std::string& name)
+		: AActor()
+	{
+	}
+
+	AStaticMeshActor::~AStaticMeshActor()
+	{
+		// Cleanup code if necessary
+	}
+}
---
+++ Karma/src/Karma/Engine/Classes/Engine/StaticMeshActor.h
@@ -0,0 +1,37 @@
+/**
+ * @file StaticMeshActor.h
+ * @brief Declaration of the StaticMeshActor class, representing an actor with a static mesh component in the game world.
+ * @author Ravi Mohan (the_cowboy)
+ * @date December 12, 2025
+ * 
+ * @copyright Karma Engine copyright(c) People of India
+ */
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+
+namespace Karma
+{
+	/**
+	 * @brief An actor that contains a static mesh component, allowing it to be rendered in the game world.
+	 * 
+	 * @since Karma 1.0.0
+	 */
+	class KARMA_API AStaticMeshActor : public AActor
+	{
+	public:
+		/**
+		 * @brief Constructor for StaticMeshActor
+		 * 
+		 * @param name The name of the static mesh actor.
+		 */
+		AStaticMeshActor(const std::string& name = "StaticMeshActor");
+		
+		/**
+		 * @brief Destructor for StaticMeshActor
+		 */
+		virtual ~AStaticMeshActor();
+		// Additional methods and members specific to StaticMeshActor can be added here.
+	};
+}
